4f3522dbe69ca642413f8bdb047c065702c0f7bc
Guildera <admin@guildera.ai>
2026-09-22 00:13:41 +0800

    feat: use larger viewports (2K-4K) to see more posts per scroll

--- scraper.js
+++ scraper.js
@@ -82,8 +82,8 @@ function parseEngagementNum(str) {
   }
 
   const viewports = [
-    { width: 1366, height: 768 }, { width: 1920, height: 1080 }, { width: 1536, height: 864 },
-    { width: 1440, height: 900 }, { width: 1280, height: 800 }, { width: 1600, height: 900 }
+    { width: 1920, height: 1080 }, { width: 2560, height: 1440 }, { width: 3840, height: 2160 },
+    { width: 3440, height: 1440 }, { width: 2560, height: 1080 }, { width: 3840, height: 1600 }
   ];
   const viewport = viewports[Math.floor(Math.random() * viewports.length)];
   const contextOptions = { viewport };
